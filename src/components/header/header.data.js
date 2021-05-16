57ddbf11b3158bb880088674670ebb1c6d30a065
Prarthan Balaji <prarthanb1993@gmail.com>
2021-05-16 20:14:32 +0530

    Portfolio Created Using React and Nextjs

---
+++ src/components/header/header.data.js
@@ -0,0 +1,22 @@
+export default [{
+        path: 'home',
+        label: 'Home',
+    },
+    {
+        path: 'feature',
+        label: 'Feature',
+    },
+    {
+        path: 'skills',
+        label: 'Skills',
+    },
+    {
+        path: 'services',
+        label: 'Services',
+    },
+    {
+        path: 'pricing',
+        label: 'Pricing',
+    },
+
+];
